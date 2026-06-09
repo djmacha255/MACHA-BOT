0828b6e4dfacdb2737eb55a8b590852d11d49e57
djmacha255 <168182335+djmacha255@users.noreply.github.com>
2026-06-09 12:04:11 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'MACHA-BOT',
+  author: '‎',
+  botName: "MACHA-BOT",
+  botOwner: 'DJ MACHA 255', // Your name
+  ownerNumber: '255612801118', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
